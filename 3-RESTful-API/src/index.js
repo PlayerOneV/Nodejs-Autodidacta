f6db8a461773fb67a697b1c91e044313527c28c1
PlayerOne <player_one_117@outlook.com>
2022-04-21 02:30:13 -0500

    Rutas, get y errores

--- 3-RESTful-API/src/index.js
+++ 3-RESTful-API/src/index.js
@@ -3,12 +3,27 @@ const express = require('express') //iniciamos un objeto para utilizar express
 const app = express() //Creamos una "app"
 
 const personRoute = require('./routes/person') //Importamos la ruta de person.js
-
+const path = require('path')
+app.use((req, res, next) => {
+    console.log(`${new Date().toString()} => ${req.originalUrl}`)
+    next()
+})
 app.use(personRoute) //Registramos la ruta
 app.use(express.static('public'))
 
-const PORT = process.env.PORT || 5000
+//Error 404 - Resource no found
+app.use((req, res, next) => {
+    res.status(404).send('Error 404 Page no found.')
+})
 
+//Error 500 - 
+app.use((err, req, res, next) => {
+    console.log(err.stack)
+
+    res.sendFile(path.join(__dirname, '../public/500.html'))
+})
+
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log('The server is on the port: '+ PORT)
 })
--- 3-RESTful-API/src/routes/person.js
+++ 3-RESTful-API/src/routes/person.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+//QueryString => query property on the request object
+//localhost:5000/person?name=Auri&age=19
 router.get('/person', (req, res) => {
-    res.send('You have requested a person')
+    if (req.query.name) {
+        res.send(`You have requested a person, parametro ${req.query.name}`)
+    } else {
+        res.send('You have requested a person, here you got budy')
+    }
+})
+
+//Params property on the request object
+//localhost:5000/person/Auri
+router.get('/person/:name', (req, res) => {
+    res.send(`You have requested a person, parametro ${req.params.name}`)
+})
+
+router.get('/error', (req, res) => {
+    throw new Error('This is a forced error')
 })
 
 module.exports = router
